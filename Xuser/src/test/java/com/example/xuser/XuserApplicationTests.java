836495fbb741c14294d85264ec0bed56db2a34b4
ha vu <doilagivayta050@gmail.com>
2022-07-27 09:52:22 +0700

    Initial commit

---
+++ Xuser/src/test/java/com/example/xuser/XuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
